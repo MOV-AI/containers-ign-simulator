277c72ca4ee5570ed5d382e741f2f7b7188b1e5d
Alex Fernandes <84147256+AlexFernandes-MOVAI@users.noreply.github.com>
2022-04-27 00:18:08 +0100

    Feat : review repo and cicd for public access (#11)
    * review repo and cicd for public access
    
    * change base repo
    
    * update readme
    
    * Update README.md
    
    * Delete dependabot.yml
    
    * move file
    
    * Update README.md
    
    * bumping version

--- gazebo/movai-entrypoint.sh
+++ gazebo/movai-entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+#
+# Copyright 2021 MOV.AI
+#
+#    Licensed under the Mov.AI License version 1.0;
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.mov.ai/flow-license/
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
 # File: movai-entrypoint.sh
 set -e
-printf "Mov.ai IGN Simulator - %s Edition\n" "$MOVAI_ENV"
+printf "MOV.AI IGN Simulator - %s\n" "$MOVAI_ENV"
 
 export PATH=${MOVAI_HOME}/.local/bin:${PATH}
 export PYTHONPATH=${APP_PATH}:${MOVAI_HOME}/sdk:${PYTHONPATH}
