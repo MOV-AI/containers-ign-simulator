23484523a3fb3d7d4c5fdea58ece69f174279748
Mariana Alves <mariana.alves@mov.ai>
2023-11-15 16:35:43 +0000

    Comment security fix

--- simulator_api/entrypoint.py
+++ simulator_api/entrypoint.py
@@ -3,8 +3,11 @@ from simulator_api.rest_server.exposed_methods import commands
 
 app = Flask(__name__)
 app.register_blueprint(commands)
-app.config['WTF_CSRF_ENABLED'] = True  # Sensitive
+# TODO: fix CSRF security issue
+# from flask_wtf.csrf import CSRFProtect
+# app.config.update(SECRET_KEY="hello")
+# csrf = CSRFProtect()
+# csrf.init_app(app)  # Compliant
 
 if __name__ == '__main__':
-
     app.run(port=8081)
