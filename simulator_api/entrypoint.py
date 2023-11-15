a99bbedfba76a4ef560feec2bec15da731a4b795
Mariana Alves <mariana.alves@mov.ai>
2023-11-15 14:57:45 +0000

    Add linting checks

--- simulator_api/entrypoint.py
+++ simulator_api/entrypoint.py
@@ -3,6 +3,7 @@ from simulator_api.rest_server.exposed_methods import commands
 
 app = Flask(__name__)
 app.register_blueprint(commands)
+app.config['WTF_CSRF_ENABLED'] = False  # Sensitive
 
 if __name__ == '__main__':
 
