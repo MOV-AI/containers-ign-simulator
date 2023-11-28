ad2cf40e8db5e82054d55151593e02964527005f
mariana-dias-alves <139454338+mariana-dias-alves@users.noreply.github.com>
2023-11-28 09:18:23 +0000

    DP 1320 - add smoke tests to simulator (#58)
    * Create web application
    
    * Update handle function to test ignition
    
    * Comment code
    
    * Update build requirements
    
    * Fix typo
    
    * Gather all topics
    
    * Fix definition of handle function
    
    * Use ICommand module instead of aiohttp
    
    * simplify handle function
    
    * Remove unnecessary dependencies
    
    * Turn handle functions into statis
    
    * Add timeout flag
    
    * Package the web server application
    
    * enable versioning apis
    
    * Change output format
    
    * Add error exception catch
    
    * Add ignition thread
    
    * Kill the ign process
    
    * Remove threading
    
    * Remove threading
    
    * Allow for no version in api request
    
    * Update webservercore version
    
    * Turn requests into async
    
    * Fix bug on logging conflict between celery and flask
    
    * Add echo and publish endpoints
    
    * Change class names
    
    * Change backend used due to bug in rpc
    
    * Change location of db
    
    * Add intermediary results for celery tasks
    
    * Add urlspecific argument to handler functions
    
    * Convert info logs into debug logs
    
    * Add config file
    
    * Restrict versions of dependencies
    
    * Add docstrings to functions
    
    * Fix url conflict
    
    * Fix config bug
    
    * Add supervisord to manage services
    
    * Fix config bug
    
    * Fix Docerfile warnings on install
    
    * Save celery database in dedicated volume
    
    * Fix dockerfile bug
    
    * Delete unnecessary functions
    
    * Change gunicorn service permissions
    
    * Add url call tests
    
    * Add commands tests
    
    * Refactor and add tests on celery tasks
    
    * Refactor to fix celery auto discovery
    
    * Complete docstring
    
    * Fix docstrings
    
    * Fix typo
    
    * Change folder name
    
    * Fix install bug
    
    * Fix run bug
    
    * Adjust dockerfile according to syntax suggested
    
    * Add versions to apt packages
    
    * Add option to choose comm test params
    
    * Update documentation on endpoints
    
    * plug the CICD for python syntax verification
    
    * Add build requirements
    
    * Add check for optional params in comm test
    
    * Fix tests
    
    * Add linting checks
    
    * Fix linting
    
    * Linting
    
    * Fix security issue
    
    * Fix lint
    
    * Fix lint
    
    * Fix pylint
    
    * Fix test
    
    * Comment security fix
    
    * review supervisord conf and readme
    
    * Fix startup issue on celery
    
    * Correct api outputs
    
    * Change task json format
    
    * fix entrypoint and pidfile wait
    
    * Fix tests
    
    * pylint fixes
    
    * add Sonar badge
    
    * Fix permission error on celery
    
    * Catch Celery IntegrityError
    
    * Remove task status update to fix database conflict
    
    * Change user of ignition
    
    * Flake8 corrections
    
    * review of Dockerfile
    
    * Add finished tasks
    
    * Get all task ids from api database
    
    * Use sqlalchemy instead of sqlite3
    
    * Add state to task id db
    
    * Add param verification tests
    
    * Fix verification of tasks
    
    * Clean tests
    
    * Fix build
    
    * Correct folder to find package
    
    * Update version
    
    * fix line length and bumpversion
    
    * more linting
    
    * review CICD up to deploy to main
    
    * Update platforms
    
    * Update from dev
    
    * Add IGN_IP to celery processes
    
    * Add ign_relay to celery tasks
    
    ---------
    
    Co-authored-by: duartecoelhomovai <duarte@mov.ai>
    Co-authored-by: OttoMation-Movai <alex.fernandes@mov.ai>

--- docker/start-celery.sh
+++ docker/start-celery.sh
@@ -6,5 +6,4 @@ LOG_LEVEL=${LOG_LEVEL:-info}
 rabbitmqctl wait $RABBITMQ_PID_FILE
 
 # Start Celery worker after RabbitMQ is ready
-# -w option is needed not to reset environment variables in the workers process
-su -w IGN_PARTITION - movai -c "celery -A simulator_api.celery_tasks.tasks.celery_instance worker --concurrency=2 --loglevel=$LOG_LEVEL"
+su -w IGN_PARTITION -w IGN_IP -w IGN_RELAY - movai -c "celery -A simulator_api.celery_tasks.tasks.celery_instance worker --concurrency=2 --loglevel=$LOG_LEVEL"
